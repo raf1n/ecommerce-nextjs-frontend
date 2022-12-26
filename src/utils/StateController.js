c64ec19d14a7e2caf17a5debc13c43b038b5d1da
sayem mahmud <sayem.mahmud97@gmail.com>
2022-12-27 01:28:01 +0600

    -ml

--- src/utils/StateController.js
+++ src/utils/StateController.js
@@ -6,7 +6,7 @@ const composeEnhancers =
     typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
         ? // @ts-ignore
         window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-            // Specify extension’s options like name, actionsDenylist, actionsCreators, serialize...
+            // Speciffy extension’s options like name, actionsDenylist, actionsCreators, serialize...
         })
         : compose;
 
