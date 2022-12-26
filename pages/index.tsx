41d26b2610b7d936404d43a978749cdfed7f43c5
sayem mahmud <sayem.mahmud97@gmail.com>
2022-12-27 01:13:29 +0600

    -ml

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,21 @@
+import { useSelector } from "react-redux";
+import { controller } from "../src/utils/StateController";
 
 
 const index = () => {
-  // const states = useSelector(() => controller.states);
+  const states = useSelector(() => controller.states);
 
   return <div>
     <p>Counter
-      {/* {states.counter} */}
+      {states.counter}
     </p>
       <button onClick={() => {
-          // controller.increase()
+          controller.increase()
       }}>+</button>
       <button onClick={() => {
           //
           // doSomething()
+          controller.decrease()
       }}>-</button>
   </div>
 }
