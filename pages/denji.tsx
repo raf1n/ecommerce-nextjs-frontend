7103a741207f9f0c13c27f3afdd6179ac80889b5
Rahim Uddin Rafin <rahim.rafin.1@gmail.com>
2023-01-03 17:42:43 +0600

    categorized-item-added

--- pages/denji.tsx
+++ pages/denji.tsx
@@ -3,10 +3,10 @@ import { useSelector } from "react-redux";
 import { controller } from "../src/state/StateController";
 import HeroSection from "../components/pages/homepage/herosection/HeroSection";
 import ProductCategory from "../components/pages/homepage/ProductCategory/ProductCategory";
-import Banner1 from "../components/pages/homepage/Banners/Banner1";
 import SingleProduct from "../components/pages/singleproduct/SingleProduct";
 import Contact from "../components/pages/contact/Contact";
 import Blogs from "../components/pages/blogs/Blogs";
+import CategorizedItem from "../components/pages/categorizeditems/CategorizedItems";
 
 interface Props {}
 
@@ -17,10 +17,10 @@ const denji: React.FC<Props> = (props) => {
     <>
       <HeroSection></HeroSection>
       <ProductCategory />
-      <Banner1 />
       <SingleProduct />
       <Contact />
       <Blogs />
+      <CategorizedItem></CategorizedItem>
     </>
   );
 };
