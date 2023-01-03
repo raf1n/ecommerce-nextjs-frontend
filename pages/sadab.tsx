778e82c1aa3908f67d1be84891b8037f64172857
Mubtasimur Rahman <sadabrahman275@gmail.com>
2023-01-03 13:42:04 +0600

    item details done

--- pages/sadab.tsx
+++ pages/sadab.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import Banner1 from '../components/pages/homepage/Banners/Banner1'
+import Itemdetails from '../components/pages/homepage/itemdetails/Itemdetails'
 import ProductCategory from '../components/pages/homepage/ProductCategory/ProductCategory'
 import Shopbybrand from '../components/pages/homepage/shopbybrand/Shopbybrand'
 import Toprated from '../components/pages/homepage/topratedproducts/Toprated'
@@ -19,6 +20,7 @@ const sadab: React.FC<Props> = (props) => {
             <Toprated />
             {/* <ProductCategory />
             <Banner1 /> */}
+            <Itemdetails />
         </>
     )
 }
