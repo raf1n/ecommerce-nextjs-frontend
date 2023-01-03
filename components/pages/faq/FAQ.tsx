01cf3f87f78bc166fcc3dc21655c63a3f972f771
Toukir Ahammed <ahmdtoukir@gmail.com>
2023-01-03 17:14:56 +0600

    have any questions (faqRight) done

--- components/pages/faq/FAQ.tsx
+++ components/pages/faq/FAQ.tsx
@@ -1,19 +1,29 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { controller } from '../../../src/state/StateController'
-import FaqHeader from './FaqHeader'
+import React from "react";
+import { useSelector } from "react-redux";
+import { controller } from "../../../src/state/StateController";
+import FaqHeader from "./FaqHeader";
+import FaqRight from "./FaqRight";
 
-interface Props {
-}
+interface Props {}
 
 const FAQ: React.FC<Props> = (props) => {
-  const states = useSelector(() => controller.states)
-  
+  const states = useSelector(() => controller.states);
+
   return (
     <div>
       <FaqHeader />
+      <div className="w-full mb-10">
+        <div className="container-x mx-auto w-full lg:flex lg:gap-x-[30px]">
+          <div className="lg:w-1/2 w-full mb-10 lg:mb-0">
+            {/* <ItemDetailsLeft></ItemDetailsLeft> */}
+          </div>
+          <div className="flex-1">
+            <FaqRight />
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FAQ
+export default FAQ;
