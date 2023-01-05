6d18934164d98a21b96fb68d4748c5ed61f07366
Iqbal <iamhasan9501@gmail.com>
2023-01-05 16:26:05 +0600

    related product cart solved

--- components/pages/Admin/Dashboard/Dashboard.tsx
+++ components/pages/Admin/Dashboard/Dashboard.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { controller } from "../../../../src/state/StateController";
+import Table from "../../../shared/Table/Table";
 
 interface Props {}
 
 const Dashboard: React.FC<Props> = (props) => {
   const states = useSelector(() => controller.states);
 
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <Table />
+    </div>
+  );
 };
 
 export default Dashboard;
--- pages/richit/ironman.tsx
+++ pages/richit/ironman.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import Dashboard from "../../components/pages/Admin/Dashboard/Dashboard";
 import DeliveryComponents from "../../components/pages/Homepage/DeliveryPolicyData/DeliveryComponents";
 import FeaturedProducts from "../../components/pages/Homepage/FeaturedProducts/FeaturedProducts";
 import NewReleasedProducts from "../../components/pages/Homepage/NewReleasedProducts/NewReleasedProducts";
 import PopularCategory from "../../components/pages/Homepage/PopularCategory/PopularCategory";
+import RelatedProductSection from "../../components/pages/Homepage/RelatedProductSection/RelatedProductSection";
 import Wishlist from "../../components/pages/Wishlist/Wishlist";
 import { controller } from "../../src/state/StateController";
 
@@ -19,6 +21,8 @@ const ironman: React.FC<Props> = (props) => {
       <NewReleasedProducts></NewReleasedProducts>
       <PopularCategory></PopularCategory>
       <Wishlist></Wishlist>
+      <RelatedProductSection></RelatedProductSection>
+      <Dashboard></Dashboard>
     </div>
   );
 };
