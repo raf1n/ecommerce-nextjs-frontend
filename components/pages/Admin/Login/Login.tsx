3bf94b0b35a10f3be95628881938406693ac21b3
Iqbal <iamhasan9501@gmail.com>
2023-01-06 23:30:59 +0600

    login form top border solved

--- components/pages/Admin/Login/Login.tsx
+++ components/pages/Admin/Login/Login.tsx
@@ -23,7 +23,7 @@ const Login: React.FC<Props> = (props) => {
           </a>
           <div
             className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
-            style={{ borderTop: "6px solid blue" }}>
+            style={{ borderTop: "2px solid blue" }}>
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                 Login
@@ -33,7 +33,7 @@ const Login: React.FC<Props> = (props) => {
                   <label
                     htmlFor="email"
                     className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                    Your email
+                    Email
                   </label>
                   <input
                     type="email"
