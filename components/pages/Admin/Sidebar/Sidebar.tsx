6ccad42e013a92459c7c41a032056ef1144a9843
comconToukir <ahmdtoukir@gmail.com>
2023-01-06 15:37:35 +0600

    added responsiveness

--- components/pages/Admin/Sidebar/Sidebar.tsx
+++ components/pages/Admin/Sidebar/Sidebar.tsx
@@ -15,16 +15,28 @@ const Sidebar: React.FC<Props> = (props) => {
 
   const [open, setOpen] = useState(true);
   const [menuOpen, setMenuOpen] = useState(0);
+  const [responsiveOpen, setResponsiveOpen] = useState(false);
 
   return (
     <div
       className={` ${
         open ? "w-[250px]" : "w-[65px] "
-      } bg-white relative duration-500`}
+      } ${ responsiveOpen ? "left-0" : "left-[-250px]"} fixed  lg:left-0 lg:relative bg-white duration-500`}
     >
       <FaBars
-        className={`absolute cursor-pointer -right-12 top-6 w-7 rounded-full duration-300`}
-        onClick={() => setOpen(!open)}
+        className={`absolute cursor-pointer hidden lg:block -right-12 top-6 w-7 rounded-full duration-300`}
+        onClick={() => {
+          setOpen(!open);
+          setResponsiveOpen(false);
+        }}
+      />
+
+      <FaBars
+        className={` left-[270px] absolute cursor-pointer block lg:hidden top-6 w-7 rounded-full duration-300`}
+        onClick={() => {
+          setResponsiveOpen(!responsiveOpen);
+          setOpen(true)
+        }}
       />
 
       <div className="text-center h-[60px] leading-[60px]">
