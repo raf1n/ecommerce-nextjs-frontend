d9ba2650dca2086723f09bbc6e0b506fe4902d35
Rahim Uddin Rafin <rahim.rafin.1@gmail.com>
2023-01-08 23:52:18 +0600

    contact-page-ui-fixed

--- components/pages/ContactPage/Contact.tsx
+++ components/pages/ContactPage/Contact.tsx
@@ -5,9 +5,7 @@ import PageHeader from "../../shared/SharedPageHeader/PageHeader";
 import ContactInformation from "./ContactInformation";
 import GetInTouch from "./GetInTouchComponent/GetInTouch";
 
-
-
-interface Props { }
+interface Props {}
 
 const Contact: React.FC<Props> = (props) => {
   const states = useSelector(() => controller.states);
@@ -15,14 +13,15 @@ const Contact: React.FC<Props> = (props) => {
   return (
     <>
       <PageHeader slug="Contact" link="/contact" title="Contact" />
-      <div className="w-full min-h-screen  pt-0 pb-0">
+      <div className="w-full min-h-screen mt-10 pt-0 pb-0">
         <div className="container-x mx-auto">
           <div className="main-wrapper w-full lg:flex lg:space-x-[30px]">
-            <div> <ContactInformation></ContactInformation> </div>
+            <ContactInformation></ContactInformation>
             <GetInTouch />
           </div>
         </div>
-      </div></>
+      </div>
+    </>
   );
 };
 
