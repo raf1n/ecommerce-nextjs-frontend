29c42474b7210663c52c5ad7f64c37c7d3bd8262
Iqbal <iamhasan9501@gmail.com>
2023-01-09 17:29:48 +0600

    table striped done

--- components/shared/SharedTable/Table.tsx
+++ components/shared/SharedTable/Table.tsx
@@ -9,8 +9,8 @@ const Table: React.FC<Props> = (props) => {
   const tableArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   return (
-    <div style={{ margin: "20px" }}>
-      <div className="bg-white p-4 rounded-md w-full">
+    <div style={{ margin: "20px", backgroundColor: "white" }}>
+      <div className=" p-4 rounded w-full">
         <div className=" flex items-center justify-between pb-6">
           <div>
             <span className="text-xs px-1 ">Show </span>
@@ -50,7 +50,7 @@ const Table: React.FC<Props> = (props) => {
         </div>
         <div>
           <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-1 overflow-x-auto">
-            <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
+            <div className="inline-block min-w-full shadow rounded overflow-hidden">
               <table className="min-w-full leading-normal">
                 <thead>
                   <tr>
@@ -86,91 +86,85 @@ const Table: React.FC<Props> = (props) => {
                 {/* -----------Plz Attention ,Table body/Row start here -------------- */}
                 <tbody>
                   {tableArray.map((tab, index) => (
-                    <>
-                      <tr>
-                        <td className="px-3 py-3 border-b border-gray-200 bg-white text-sm">
-                          <p className="text-gray-900 whitespace-no-wrap">
-                            {index + 1}
-                          </p>
-                        </td>
-                        <td className="px-3 py-3 border-b border-gray-200 bg-white text-sm">
-                          <p className="text-gray-900 whitespace-no-wrap">
-                            Rumon
-                          </p>
-                        </td>
-                        <td className="px-3 py-3 border-b border-gray-200 bg-white text-sm">
-                          <p className="text-gray-900 whitespace-no-wrap">
-                            512
-                          </p>
-                        </td>
-                        <td className="px-3 py-3 border-b border-gray-200 bg-white text-sm">
-                          <p className="text-gray-900 whitespace-no-wrap">
-                            10-5-2023
-                          </p>
-                        </td>
-                        <td className="px-3 py-3 border-b border-gray-200 bg-white text-sm">
-                          <p className="text-gray-900 whitespace-no-wrap">
-                            420
-                          </p>
-                        </td>
-                        <td className="px-3 py-3 border-b border-gray-200 bg-white text-sm">
-                          <p className="text-gray-900 whitespace-no-wrap">
-                            10200
-                          </p>
-                        </td>
+                    <tr className="even:bg-gray-50 odd:bg-white">
+                      <td className="px-3 py-3    text-sm">
+                        <p className="text-gray-900 whitespace-no-wrap">
+                          {index + 1}
+                        </p>
+                      </td>
+                      <td className="px-3 py-3  text-sm">
+                        <p className="text-gray-900 whitespace-no-wrap">
+                          Rumon
+                        </p>
+                      </td>
+                      <td className="px-3 py-3   text-sm">
+                        <p className="text-gray-900 whitespace-no-wrap">512</p>
+                      </td>
+                      <td className="px-3 py-3 text-sm">
+                        <p className="text-gray-900 whitespace-no-wrap">
+                          10-5-2023
+                        </p>
+                      </td>
+                      <td className="px-3 py-3  text-sm">
+                        <p className="text-gray-900 whitespace-no-wrap">420</p>
+                      </td>
+                      <td className="px-3 py-3  text-sm">
+                        <p className="text-gray-900 whitespace-no-wrap">
+                          10200
+                        </p>
+                      </td>
 
-                        <td className="px-3 py-3 border-b border-gray-200 bg-white text-sm">
-                          <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
-                            <span
-                              aria-hidden
-                              className="absolute inset-0 bg-red-500  rounded-full"></span>
-                            <span className="relative text-white text-xs">
-                              Pending
-                            </span>
+                      <td className="px-3 py-3   text-sm">
+                        <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+                          <span
+                            aria-hidden
+                            className="absolute inset-0 bg-red-500  rounded"></span>
+                          <span className="relative text-white text-xs">
+                            Pending
                           </span>
-                        </td>
+                        </span>
+                      </td>
 
-                        <td className="px-3 py-3 border-b border-gray-200 bg-white text-sm">
-                          <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
-                            <span
-                              aria-hidden
-                              className="absolute inset-0 bg-green-500 rounded-full"></span>
-                            <span className="relative text-white text-xs">
-                              Success
-                            </span>
+                      <td className="px-3 py-3 text-sm">
+                        <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+                          <span
+                            aria-hidden
+                            className="absolute inset-0 bg-green-500 rounded"></span>
+                          <span className="relative text-white text-xs">
+                            Success
                           </span>
-                        </td>
+                        </span>
+                      </td>
 
-                        <td className="px-3 py-3 border-b border-gray-200 bg-white text-sm">
-                          <button>
-                            <span className="relative inline-block px-1 py-1 font-semibold text-green-900 leading-tight">
-                              <span className="h-8 w-8  inset-0 bg-blue-700   rounded  relative text-white flex justify-center items-center">
-                                <FaEye />
-                              </span>
-                            </span>
-                          </button>
-                          <button>
-                            <span className="relative inline-block px-1 py-1 font-semibold text-green-900 leading-tight">
-                              <span className="h-8 w-8  inset-0 bg-red-500   rounded  relative text-white flex justify-center items-center">
-                                <FaTrash />
-                              </span>
-                            </span>
-                          </button>
+                      <td className="px-3 py-3  text-sm">
+                        <button>
                           <span className="relative inline-block px-1 py-1 font-semibold text-green-900 leading-tight">
-                            <button>
-                              <span className="h-8 w-8  inset-0 bg-orange-400   rounded  relative text-white flex justify-center items-center">
-                                <FaTruck />
-                              </span>
-                            </button>
+                            <span className="h-8 w-8  inset-0 bg-blue-700   rounded  relative text-white flex justify-center items-center">
+                              <FaEye />
+                            </span>
                           </span>
-                        </td>
-                      </tr>
-                    </>
+                        </button>
+                        <button>
+                          <span className="relative inline-block px-1 py-1 font-semibold text-green-900 leading-tight">
+                            <span className="h-8 w-8  inset-0 bg-red-500   rounded  relative text-white flex justify-center items-center">
+                              <FaTrash />
+                            </span>
+                          </span>
+                        </button>
+                        <span className="relative inline-block px-1 py-1 font-semibold text-green-900 leading-tight">
+                          <button>
+                            <span className="h-8 w-8  inset-0 bg-orange-400   rounded  relative text-white flex justify-center items-center">
+                              <FaTruck />
+                            </span>
+                          </button>
+                        </span>
+                      </td>
+                    </tr>
                   ))}
                 </tbody>
               </table>
               {/* -------------- */}
-              <div className="px-5 py-5 bg-white border-t flex justify-between">
+              <div className="px-5 py-5  border-t flex justify-between">
                 <span className="text-xs xs:text-sm text-gray-900">
                   Showing 1 to 4 of 50 Entries
                 </span>
