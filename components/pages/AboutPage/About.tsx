faefcb784be5fe990016d7bb3a134857f7171095
Mubtasimur Rahman <sadabrahman275@gmail.com>
2023-01-10 13:27:42 +0600

    about us part two slidely done

--- components/pages/AboutPage/About.tsx
+++ components/pages/AboutPage/About.tsx
@@ -4,6 +4,10 @@ import { controller } from '../../../src/state/StateController'
 import PageHeader from '../../shared/SharedPageHeader/PageHeader'
 import { AiFillCheckCircle } from 'react-icons/ai';
 import styles from './About.module.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { Component } from "react";
+import Slider from "react-slick";
 
 interface Props {
 }
@@ -11,6 +15,51 @@ interface Props {
 const About: React.FC<Props> = (props) => {
 
     const states = useSelector(() => controller.states)
+    const feedbacks = [
+        {
+            description: "Almost every imaginable design is possible and customizations are allowed on every level. Some features could make use of better controls. If you know how to operate your mouse, then you are all set to use this pagebuilder.",
+            name: "Michael",
+            designation: "CEO",
+            picture: "https://shopo-ecom.vercel.app/_next/image?url=https%3A%2F%2Fapi.websolutionus.com%2Fshopo%2Fuploads%2Fcustom-images%2Fjohn-doe-20220922033955.png&w=1920&q=75"
+        },
+        {
+            description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English",
+            name: "James Robert",
+            designation: "CEO",
+            picture: "https://shopo-ecom.vercel.app/_next/image?url=https%3A%2F%2Fapi.websolutionus.com%2Fshopo%2Fuploads%2Fcustom-images%2Fjohn-doe-20220922033955.png&w=1920&q=75"
+        },
+        {
+            description: "As a digital marketing agency our team works day in and day out on websites of all kinds. Some of the most common errors we see are websites not optimized for SEO because of old, boring, or out of date website themes or designs.",
+            name: "John Doe",
+            designation: "CEO",
+            picture: "https://shopo-ecom.vercel.app/_next/image?url=https%3A%2F%2Fapi.websolutionus.com%2Fshopo%2Fuploads%2Fcustom-images%2Fjohn-doe-20220922033955.png&w=1920&q=75"
+        },
+        {
+            description: "Almost every imaginable design is possible and customizations are allowed on every level. Some features could make use of better controls. If you know how to operate your mouse, then you are all set to use this pagebuilder.",
+            name: "Michael",
+            designation: "CEO",
+            picture: "https://shopo-ecom.vercel.app/_next/image?url=https%3A%2F%2Fapi.websolutionus.com%2Fshopo%2Fuploads%2Fcustom-images%2Fjohn-doe-20220922033955.png&w=1920&q=75"
+        },
+        {
+            description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English",
+            name: "James Robert",
+            designation: "CEO",
+            picture: "https://shopo-ecom.vercel.app/_next/image?url=https%3A%2F%2Fapi.websolutionus.com%2Fshopo%2Fuploads%2Fcustom-images%2Fjohn-doe-20220922033955.png&w=1920&q=75"
+        },
+        {
+            description: "Almost every imaginable design is possible and customizations are allowed on every level. Some features could make use of better controls. If you know how to operate your mouse, then you are all set to use this pagebuilder.",
+            name: "Michael",
+            designation: "CEO",
+            picture: "https://shopo-ecom.vercel.app/_next/image?url=https%3A%2F%2Fapi.websolutionus.com%2Fshopo%2Fuploads%2Fcustom-images%2Fjohn-doe-20220922033955.png&w=1920&q=75"
+        },
+    ]
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1
+    };
 
     return (
         <div className=''>
@@ -87,7 +136,184 @@ const About: React.FC<Props> = (props) => {
                         </div>
                     </div>
                 </div>
+                <div className='customer-feedback w-full bg-white py-[60px]'>
+                    <div className='title flex justify-center mb-5'>
+                        <h1 className='text-[30px] font-semibold text-qblack'>Customers Feedback</h1>
+                    </div>
+                    <Slider {...settings} className='feedback-slider-wrapper  relative overflow-hidden flex  mx-10'>
 
+                        {feedbacks.map((feedback, index) => <>
+                            <div className='item h-auto sm:px-1 sm:py-9 p-10'>
+                                <div className='bg-primarygray p-10'>
+                                    <div className='rating flex space-x-1 items-center mb-4'>
+                                        <span>
+                                            <svg
+                                                className="fill-qyellow"
+                                                width="18"
+                                                height="17"
+                                                viewBox="0 0 18 17"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                            >
+                                                <path d='M9 0L11.0206 6.21885H17.5595L12.2694 10.0623L14.2901 16.2812L9 12.4377L3.70993 16.2812L5.73056 10.0623L0.440492 6.21885H6.97937L9 0Z fill="currentColor"'></path>
+                                            </svg>
+                                        </span>
+                                        <span>
+                                            <svg
+                                                className="fill-qyellow"
+                                                width="18"
+                                                height="17"
+                                                viewBox="0 0 18 17"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                            >
+                                                <path d='M9 0L11.0206 6.21885H17.5595L12.2694 10.0623L14.2901 16.2812L9 12.4377L3.70993 16.2812L5.73056 10.0623L0.440492 6.21885H6.97937L9 0Z fill="currentColor"'></path>
+                                            </svg>
+                                        </span>
+                                        <span>
+                                            <svg
+                                                className="fill-qyellow"
+                                                width="18"
+                                                height="17"
+                                                viewBox="0 0 18 17"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                            >
+                                                <path d='M9 0L11.0206 6.21885H17.5595L12.2694 10.0623L14.2901 16.2812L9 12.4377L3.70993 16.2812L5.73056 10.0623L0.440492 6.21885H6.97937L9 0Z fill="currentColor"'></path>
+                                            </svg>
+                                        </span>
+                                        <span>
+                                            <svg
+                                                className="fill-qyellow"
+                                                width="18"
+                                                height="17"
+                                                viewBox="0 0 18 17"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                            >
+                                                <path d='M9 0L11.0206 6.21885H17.5595L12.2694 10.0623L14.2901 16.2812L9 12.4377L3.70993 16.2812L5.73056 10.0623L0.440492 6.21885H6.97937L9 0Z fill="currentColor"'></path>
+                                            </svg>
+                                        </span>
+                                        <span>
+                                            <svg
+                                                className="fill-qyellow"
+                                                width="18"
+                                                height="17"
+                                                viewBox="0 0 18 17"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                            >
+                                                <path d='M9 0L11.0206 6.21885H17.5595L12.2694 10.0623L14.2901 16.2812L9 12.4377L3.70993 16.2812L5.73056 10.0623L0.440492 6.21885H6.97937L9 0Z fill="currentColor"'></path>
+                                            </svg>
+                                        </span>
+                                        <div >
+                                            <span className='text-[13px] text-qblack'>(5)</span>
+                                        </div>
+                                    </div>
+                                    <div className='text-[15px] text-qgray leading-[30px] line-clamp-6 mb-4'>
+                                        {feedback.description}
+                                    </div>
+                                    <div className='flex items-center space-x-2.5 mt-3'>
+                                        <div className='w-[50px] h-[50px] rounded-full overflow-hidden relative'>
+                                            <span>
+                                                <img src={feedback.picture} alt="" />
+                                            </span>
+                                        </div>
+                                        <div>
+                                            <p className='text-[18px] text-qblack font-medium'>{feedback.name}</p>
+                                            <p className='text-qgray text-[13px]'>{feedback.designation}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </>)
+
+                        }
+
+                        {/* <div className='item h-auto sm:px-1 sm:py-9 p-10'>
+                            <div className='bg-primarygray p-10'>
+                                <div className='rating flex space-x-1 items-center mb-4'>
+                                    <span>
+                                        <svg
+                                            className="fill-qyellow"
+                                            width="18"
+                                            height="17"
+                                            viewBox="0 0 18 17"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <path d='M9 0L11.0206 6.21885H17.5595L12.2694 10.0623L14.2901 16.2812L9 12.4377L3.70993 16.2812L5.73056 10.0623L0.440492 6.21885H6.97937L9 0Z fill="currentColor"'></path>
+                                        </svg>
+                                    </span>
+                                    <span>
+                                        <svg
+                                            className="fill-qyellow"
+                                            width="18"
+                                            height="17"
+                                            viewBox="0 0 18 17"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <path d='M9 0L11.0206 6.21885H17.5595L12.2694 10.0623L14.2901 16.2812L9 12.4377L3.70993 16.2812L5.73056 10.0623L0.440492 6.21885H6.97937L9 0Z fill="currentColor"'></path>
+                                        </svg>
+                                    </span>
+                                    <span>
+                                        <svg
+                                            className="fill-qyellow"
+                                            width="18"
+                                            height="17"
+                                            viewBox="0 0 18 17"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <path d='M9 0L11.0206 6.21885H17.5595L12.2694 10.0623L14.2901 16.2812L9 12.4377L3.70993 16.2812L5.73056 10.0623L0.440492 6.21885H6.97937L9 0Z fill="currentColor"'></path>
+                                        </svg>
+                                    </span>
+                                    <span>
+                                        <svg
+                                            className="fill-qyellow"
+                                            width="18"
+                                            height="17"
+                                            viewBox="0 0 18 17"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <path d='M9 0L11.0206 6.21885H17.5595L12.2694 10.0623L14.2901 16.2812L9 12.4377L3.70993 16.2812L5.73056 10.0623L0.440492 6.21885H6.97937L9 0Z fill="currentColor"'></path>
+                                        </svg>
+                                    </span>
+                                    <span>
+                                        <svg
+                                            className="fill-qyellow"
+                                            width="18"
+                                            height="17"
+                                            viewBox="0 0 18 17"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <path d='M9 0L11.0206 6.21885H17.5595L12.2694 10.0623L14.2901 16.2812L9 12.4377L3.70993 16.2812L5.73056 10.0623L0.440492 6.21885H6.97937L9 0Z fill="currentColor"'></path>
+                                        </svg>
+                                    </span>
+                                    <div >
+                                        <span className='text-[13px] text-qblack'>(5)</span>
+                                    </div>
+                                </div>
+                                <div className='text-[15px] text-qgray leading-[30px] line-clamp-6 mb-4'>
+                                    As a digital marketing agency our team works day in and day out on websites of all kinds. Some of the most common errors we see are websites not optimized for SEO because of old, boring, or out of date website themes or designs.
+                                </div>
+                                <div className='flex items-center space-x-2.5 mt-3'>
+                                    <div className='w-[50px] h-[50px] rounded-full overflow-hidden relative'>
+                                        <span>
+                                            <img src="https://shopo-ecom.vercel.app/_next/image?url=https%3A%2F%2Fapi.websolutionus.com%2Fshopo%2Fuploads%2Fcustom-images%2Fjohn-doe-20220922033955.png&w=1920&q=75" alt="" />
+                                        </span>
+                                    </div>
+                                    <div>
+                                        <p className='text-[18px] text-qblack font-medium'>John Doe</p>
+                                        <p className='text-qgray text-[13px]'>CEO</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        */}
+
+
+                    </Slider>
+                    <div className=' flex justify-center mt-[40px]'>
+                        <div className='flex space-x-5 item-center'>
+                            <button type="button" className="w-[48px] h-[48px] rounded-full overflow-hidden flex justify-center items-center border border-qyellow text-qyellow focus:bg-qyellow focus:text-white"><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M15 19l-7-7 7-7"></path></svg></button>
+                            <button type="button" className="w-[48px] h-[48px] rounded-full overflow-hidden flex justify-center items-center border border-qyellow text-qyellow focus:bg-qyellow focus:text-white"><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 transform rotate-180" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M15 19l-7-7 7-7"></path></svg></button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
