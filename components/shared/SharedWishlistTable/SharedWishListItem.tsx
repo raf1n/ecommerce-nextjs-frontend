4f2a51bbc1436115176ba60803964b28f4d15200
rahatkabir007 <rahatkabir04@gmail.com>
2023-01-26 12:05:14 +0600

    data changes

--- components/shared/SharedWishlistTable/SharedWishListItem.tsx
+++ components/shared/SharedWishlistTable/SharedWishListItem.tsx
@@ -16,7 +16,7 @@ const SharedWishListItem: React.FC<Props> = ({
   return (
     <tr className="bg-white border-b hover:bg-gray-50">
       <td className="pl-10 py-4 capitalize w-[380px] ">
-        <div className="flex space-x-6 items-center">
+        <div className="flex space-x-6 rtl:space-x-reverse items-center">
           <div className="w-[80px] h-[80px] overflow-hidden flex justify-center items-center border border-[#EDEDED] relative">
             <span
               style={{
@@ -30,7 +30,6 @@ const SharedWishListItem: React.FC<Props> = ({
                 border: 0,
                 margin: 0,
                 padding: 0,
-
                 position: "absolute",
                 inset: 0,
               }}
@@ -51,10 +50,10 @@ const SharedWishListItem: React.FC<Props> = ({
                   display: "block",
                   width: 0,
                   height: 0,
-                  minWidth: 100,
-                  maxWidth: 100,
-                  minHeight: 100,
-                  maxHeight: 100,
+                  minWidth: "100%",
+                  maxWidth: "100%",
+                  minHeight: "100%",
+                  maxHeight: "100%",
                 }}
                 sizes="100vw"
               />
@@ -62,7 +61,7 @@ const SharedWishListItem: React.FC<Props> = ({
             </span>
           </div>
           <div className="flex-1 flex flex-col">
-            <p className="font-medium text-[15px] text-qblack hover:text-blue-500 cursor-pointer">
+            <p className="font-medium text-[15px] text-qblack hover:text-blue-500 cursor-pointer capitalize">
               {item.name}
             </p>
           </div>
@@ -70,7 +69,7 @@ const SharedWishListItem: React.FC<Props> = ({
       </td>
       <td className="text-center py-4 capitalize px-2">
         <div className="flex space-x-1 items-center justify-center">
-          <span className="text-[15px] font-normal">{item.offerPrice ? item.offerPrice : item.price}</span>
+          <span className="text-[15px] font-normal">${" "}{item.offerPrice ? item.offerPrice : item.price}</span>
         </div>
       </td>
       <td className="text-right py-4 capitalize">
