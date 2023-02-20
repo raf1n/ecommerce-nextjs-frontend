566bff8eacb4a343f102e2e3dfc22a35e9743354
Iqbal <iamhasan9501@gmail.com>
2023-02-20 13:12:04 +0600

    env file gayeb and backed

--- components/pages/ProfilePage/ProfileReviews/ProfileReviews.tsx
+++ components/pages/ProfilePage/ProfileReviews/ProfileReviews.tsx
@@ -1,44 +1,48 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { controller } from "./../../../../src/state/StateController";
-import ProfileReviewItem from './ProfileReviewItem';
+import ProfileReviewItem from "./ProfileReviewItem";
 
 const profileReviewsData = [
   {
     reviewDate: "23 Nov 2022",
-    productName: "Apple watch pro",
+    productName: "Apple watch pro1",
     status: "pending",
     rating: 4,
     message: "aaa",
-    imageUrl: "https://shopo-ecom.vercel.app/_next/image?url=https%3A%2F%2Fapi.websolutionus.com%2Fshopo%2Fuploads%2Fcustom-images%2Fapple-watch-pro-2022-09-26-12-04-40-6657.png&w=1920&q=75",
-    slug: "apple-watch-pro"
+    imageUrl:
+      "https://shopo-ecom.vercel.app/_next/image?url=https%3A%2F%2Fapi.websolutionus.com%2Fshopo%2Fuploads%2Fcustom-images%2Fapple-watch-pro-2022-09-26-12-04-40-6657.png&w=1920&q=75",
+    slug: "apple-watch-pro",
   },
   {
     reviewDate: "21 Nov 2022",
-    productName: "Apple watch pro",
+    productName: "Apple watch pro2",
     status: "pending",
     rating: 4,
     message: "aaa",
-    imageUrl: "https://shopo-ecom.vercel.app/_next/image?url=https%3A%2F%2Fapi.websolutionus.com%2Fshopo%2Fuploads%2Fcustom-images%2Fapple-watch-pro-2022-09-26-12-04-40-6657.png&w=1920&q=75",
-    slug: "apple-watch-pro"
+    imageUrl:
+      "https://shopo-ecom.vercel.app/_next/image?url=https%3A%2F%2Fapi.websolutionus.com%2Fshopo%2Fuploads%2Fcustom-images%2Fapple-watch-pro-2022-09-26-12-04-40-6657.png&w=1920&q=75",
+    slug: "apple-watch-pro",
   },
   {
     reviewDate: "23 Nov 2022",
-    productName: "Apple watch pro",
+    productName: "Apple watch pro3",
     status: "pending",
     rating: 4,
     message: "aaa",
-    imageUrl: "https://shopo-ecom.vercel.app/_next/image?url=https%3A%2F%2Fapi.websolutionus.com%2Fshopo%2Fuploads%2Fcustom-images%2Fapple-watch-pro-2022-09-26-12-04-40-6657.png&w=1920&q=75",
-    slug: "apple-watch-pro"
+    imageUrl:
+      "https://shopo-ecom.vercel.app/_next/image?url=https%3A%2F%2Fapi.websolutionus.com%2Fshopo%2Fuploads%2Fcustom-images%2Fapple-watch-pro-2022-09-26-12-04-40-6657.png&w=1920&q=75",
+    slug: "apple-watch-pro",
   },
   {
     reviewDate: "23 Nov 2022",
-    productName: "Apple watch pro",
+    productName: "Apple watch pro4",
     status: "approved",
     rating: 4,
     message: "aaa",
-    imageUrl: "https://shopo-ecom.vercel.app/_next/image?url=https%3A%2F%2Fapi.websolutionus.com%2Fshopo%2Fuploads%2Fcustom-images%2Fapple-watch-pro-2022-09-26-12-04-40-6657.png&w=1920&q=75",
-    slug: "apple-watch-pro"
+    imageUrl:
+      "https://shopo-ecom.vercel.app/_next/image?url=https%3A%2F%2Fapi.websolutionus.com%2Fshopo%2Fuploads%2Fcustom-images%2Fapple-watch-pro-2022-09-26-12-04-40-6657.png&w=1920&q=75",
+    slug: "apple-watch-pro",
   },
 ];
 
@@ -47,11 +51,13 @@ interface Props {}
 const ProfileReviews: React.FC<Props> = (props) => {
   const states = useSelector(() => controller.states);
 
-  return <div className="grid md:grid-cols-2 grid-cols-1 gap-8">
-    {
-      profileReviewsData.map((review, idx) => <ProfileReviewItem review={review} />)
-    }
-  </div>;
+  return (
+    <div className="grid md:grid-cols-2 grid-cols-1 gap-8">
+      {profileReviewsData.map((review, idx) => (
+        <ProfileReviewItem review={review} />
+      ))}
+    </div>
+  );
 };
 
 export default ProfileReviews;
--- components/shared/SharedProductCard/ProductCard.tsx
+++ components/shared/SharedProductCard/ProductCard.tsx
@@ -91,8 +91,7 @@ const ProductCard: React.FC<Props> = (props) => {
             className={`${styles["product-card-one"]} w-full h-[445px] bg-white relative group overflow-hidden`}
             style={{
               boxShadow: "rgba(0, 0, 0, 0.05) 0px 15px 64px 0px",
-            }}
-          >
+            }}>
             <div className="product-card-img w-full h-[300px] -mt-2">
               <div className="w-full h-full relative flex justify-center items-center transform scale-100 group-hover:scale-110 transition duration-300 ease-in-out">
                 <span
@@ -109,8 +108,7 @@ const ProductCard: React.FC<Props> = (props) => {
                     padding: 0,
                     position: "absolute",
                     inset: 0,
-                  }}
-                >
+                  }}>
                   <picture>
                     {product && product?.imageURL?.length > 0 && (
                       <img
@@ -147,11 +145,9 @@ const ProductCard: React.FC<Props> = (props) => {
                 <button
                   onClick={handleCartToggle}
                   type="button"
-                  className={`${styles["yellow-btn"]} group relative w-full h-full flex shadow justify-center items-center overflow-hidden`}
-                >
+                  className={`${styles["yellow-btn"]} group relative w-full h-full flex shadow justify-center items-center overflow-hidden`}>
                   <div
-                    className={`${styles["btn-content"]} flex items-center space-x-3 relative z-10`}
-                  >
+                    className={`${styles["btn-content"]} flex items-center space-x-3 relative z-10`}>
                     <span>
                       <SvgIconRenderer
                         width="14"
@@ -172,8 +168,7 @@ const ProductCard: React.FC<Props> = (props) => {
                     )}
                   </div>
                   <div
-                    className={`${styles["bg-shape"]} w-full h-full absolute bg-qblack`}
-                  ></div>
+                    className={`${styles["bg-shape"]} w-full h-full absolute bg-qblack`}></div>
                 </button>
               </div>
               <div className="reviews flex space-x-[1px] mb-3">
@@ -235,8 +230,7 @@ const ProductCard: React.FC<Props> = (props) => {
               </div>
               <Link
                 rel="noopener noreferrer"
-                href={`/single_product?slug=${product.slug}`}
-              >
+                href={`/single_product?slug=${product.slug}`}>
                 <p className="title mb-2 text-[15px] font-semibold text-qblack leading-[24px] line-clamp-2 hover:text-blue-600 cursor-pointer">
                   {product.productName}
                 </p>
@@ -253,8 +247,7 @@ const ProductCard: React.FC<Props> = (props) => {
             <div className="quick-access-btns flex flex-col space-y-2">
               <button
                 className=" absolute group-hover:right-4 -right-10 top-20 transition-all ease-in-out"
-                type="button"
-              >
+                type="button">
                 <span className="w-10 h-10 flex justify-center text-black hover:text-white items-center transition-all duration-300 ease-in-out hover:bg-qyellow bg-primarygray rounded">
                   <SvgIconRenderer
                     width={"20"}
@@ -272,8 +265,7 @@ const ProductCard: React.FC<Props> = (props) => {
               <button
                 className="absolute group-hover:right-4 -right-10 top-[120px] transition-all duration-300 ease-in-out"
                 type="button"
-                onClick={handleWishlist}
-              >
+                onClick={handleWishlist}>
                 <span className="w-10 h-10 flex text-black hover:text-black justify-center items-center transition-all duration-300 ease-in-out hover:bg-qyellow bg-primarygray rounded">
                   {isInWishlist(product.slug) ? (
                     <BsHeartFill
@@ -291,8 +283,7 @@ const ProductCard: React.FC<Props> = (props) => {
 
               <button
                 className="absolute group-hover:right-4 -right-10 top-[168px] transition-all duration-500 ease-in-out"
-                type="button"
-              >
+                type="button">
                 <span className="w-10 h-10 flex justify-center text-black hover:text-white transition-all duration-300 ease-in-out items-center hover:bg-qyellow bg-primarygray rounded">
                   <SvgIconRenderer
                     width={"20"}
