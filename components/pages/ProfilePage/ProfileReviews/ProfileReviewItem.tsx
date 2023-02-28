a578c4b92005701e450dc0a5a7439d5982c6efb2
Iqbal <iamhasan9501@gmail.com>
2023-02-28 15:34:26 +0600

    rating color fixed

--- components/pages/ProfilePage/ProfileReviews/ProfileReviewItem.tsx
+++ components/pages/ProfilePage/ProfileReviews/ProfileReviewItem.tsx
@@ -19,6 +19,9 @@ const ProfileReviewItem: React.FC<Props> = ({
   review: { reviewDate, reviewProducts, status, rating, message },
 }) => {
   const states = useSelector(() => controller.states);
+  console.log({
+    review: { reviewDate, reviewProducts, status, rating, message },
+  });
 
   return (
     <div
@@ -33,33 +36,38 @@ const ProfileReviewItem: React.FC<Props> = ({
           />
         </div>
 
-        {/* rating component */}
         <div className="flex-1 flex flex-col justify-center h-full">
           <div>
             <span className="text-qgray text-sm mb-1.5 block">
               {reviewDate}
             </span>
 
-            <div className="flex items-center space-x-2">
-              <ReactStars
-                count={5}
-                value={rating}
-                edit={false}
-                size={24}
-                isHalf={true}
-                emptyIcon={<FaRegStar />}
-                halfIcon={<FaStarHalfAlt />}
-                fullIcon={<FaStar />}
-                activeColor="#ffd700"
-                color="#d3d3d3"
-              />
-              <span className="text-[13px] font-normal text-qblack mt-1 inline-block">
-                ({rating})
-              </span>
-            </div>
+            {rating > 0 ? (
+              <div className="flex items-center space-x-2">
+                <ReactStars
+                  count={5}
+                  value={rating}
+                  edit={false}
+                  size={24}
+                  isHalf={true}
+                  emptyIcon={<FaRegStar />}
+                  halfIcon={<FaStarHalfAlt />}
+                  fullIcon={<FaStar />}
+                  activeColor="rgb(255, 168, 0)"
+                  color="#d3d3d3"
+                />
+                <span className="text-[13px] font-normal text-qblack mt-1 inline-block">
+                  ({rating})
+                </span>
+              </div>
+            ) : (
+              ""
+            )}
+
             <div className="flex mb-1.5"></div>
-            <Link href={`/single_product/slug=${reviewProducts.product_slug}`}>
-              <h6 className="title mb-2 sm:text-[15px] text-[13px] font-600 text-qblack leading-[24px] line-clamp-1 hover:text-blue-600 cursor-pointer">
+            <Link
+              href={`http://localhost:3000/single_product?slug=${reviewProducts.slug}`}>
+              <h6 className=" mb-2 sm:text-[15px] text-[13px] font-bold leading-[24px] line-clamp-1 hover:text-blue-600 cursor-pointer">
                 {reviewProducts.productName}
               </h6>
             </Link>
@@ -68,12 +76,6 @@ const ProfileReviewItem: React.FC<Props> = ({
             </p>
           </div>
         </div>
-
-        {status === "pending" ? (
-          <div className="absolute right-3 font-medium top-3 px-3 py-1 rounded bg-[#fff6dc] bg-opacity-50 text-qyellow border text-sm border-qyellow">
-            Pending review
-          </div>
-        ) : null}
       </div>
     </div>
   );
