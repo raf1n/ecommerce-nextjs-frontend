2ed5ff5b94f045a61539e808aa4b5ce6ff12ed83
Rahim Uddin Rafin <rahim.rafin.1@gmail.com>
2023-03-22 14:19:14 +0600

    popular-featured-card-h-w-bug-fixed

--- components/pages/HomePagee/PopularCategory/PopularCategory.tsx
+++ components/pages/HomePagee/PopularCategory/PopularCategory.tsx
@@ -32,17 +32,21 @@ const PopularCategory: React.FC<Props> = (props) => {
     <div>
       <div className="section-wrapper w-full md:mb-[60px] mb-[30px]">
         <div className="container-x mx-auto">
-          <SectionHeader title="Popular Category" link="/products?highlight=popular_category" />
+          <SectionHeader
+            title="Popular Category"
+            link="/products?highlight=popular_category"
+          />
           <div className="section-content">
             <div className="products-section w-full">
               <div className="grid xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 xl:gap-[30px] gap-5">
                 <div className="category-card hidden xl:block w-full">
                   <div
-                    className="category-card-wrappwer w-full h-full p-[30px]"
+                    className="category-card-wrappwer w-full h-[62vh] p-[30px]"
                     style={{
                       background:
                         "url(https://api.websolutionus.com/shopo/uploads/website-images/popular-cat-banner-2022-09-20-01-15-44-7577.jpg) 0% 0% / cover no-repeat",
-                    }}>
+                    }}
+                  >
                     <div>
                       <h1 className="text-base font-semibold tracking-wide mb-2">
                         Popular Category
@@ -54,7 +58,8 @@ const PopularCategory: React.FC<Props> = (props) => {
                               <li>
                                 <span
                                   onClick={() => setSlug(singlePop?.cat_slug)}
-                                  className="text-sm text-qgray hober:text-qBlack border-b border-transparent hover:border-qblack hover:text-qblack capitalize cursor-pointer">
+                                  className="text-sm text-qgray hober:text-qBlack border-b border-transparent hover:border-qblack hover:text-qblack capitalize cursor-pointer"
+                                >
                                   {singlePop.cat_name}
                                 </span>
                               </li>
@@ -94,21 +99,24 @@ const PopularCategory: React.FC<Props> = (props) => {
                             height="11"
                             viewBox="0 0 7 11"
                             fill="none"
-                            xmlns="http://www.w3.org/2000/svg">
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
                             <rect
                               x="2.08984"
                               y="0.636719"
                               width="6.94219"
                               height="1.54271"
                               transform="rotate(45 2.08984 0.636719)"
-                              fill="#1D1D1D"></rect>
+                              fill="#1D1D1D"
+                            ></rect>
                             <rect
                               x="7"
                               y="5.54492"
                               width="6.94219"
                               height="1.54271"
                               transform="rotate(135 7 5.54492)"
-                              fill="#1D1D1D"></rect>
+                              fill="#1D1D1D"
+                            ></rect>
                           </svg>
                         </span>
                       </div>
