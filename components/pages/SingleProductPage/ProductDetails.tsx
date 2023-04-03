31eb59d268247c3160639920f66e001a6a5d34d5
Rahim Uddin Rafin <rahim.rafin.1@gmail.com>
2023-04-03 17:29:58 +0600

    single-product-review-start-and-number-of-revies-added

--- components/pages/SingleProductPage/ProductDetails.tsx
+++ components/pages/SingleProductPage/ProductDetails.tsx
@@ -17,6 +17,9 @@ import { CartHandler } from "../../../src/utils/CartHandler";
 import { CookiesHandler } from "../../../src/utils/CookiesHandler";
 import { BsHeart, BsHeartFill } from "react-icons/bs";
 import toast from "react-hot-toast";
+import { FaRegStar, FaStar, FaStarHalfAlt } from "react-icons/fa";
+//@ts-ignore
+import ReactStars from "react-rating-stars-component";
 
 interface Props {
   // brand: string;
@@ -30,8 +33,29 @@ const ProductDetails: React.FC<Props> = (props) => {
   const { singleProduct } = props;
   const states = useSelector(() => controller.states);
   const [brandName, setBrandName] = useState<string | undefined>("");
+  const [avgRating, setAvgRating] = useState(0);
+  const [totalReview, setTotalReview] = useState(0);
 
   let selectedItem: ICartProduct | undefined;
+
+  const getProductReviews = async () => {
+    let rating = 0;
+    const { res, err } = await EcommerceApi.getAllProductReviews(
+      singleProduct?.slug
+    );
+    if (res) {
+      setTotalReview(res.length);
+      res.map((data) => {
+        rating = rating + data.rating / res.length;
+        if (rating / 0.5 !== 0) {
+          setAvgRating(rating);
+        } else {
+          rating = Math.floor(rating * 2) / 2;
+          setAvgRating(rating);
+        }
+      });
+    }
+  };
 
   if (singleProduct) {
     selectedItem = states?.cartlistData?.find(
@@ -53,6 +77,7 @@ const ProductDetails: React.FC<Props> = (props) => {
       }
     };
     handleBrand();
+    getProductReviews();
   }, [singleProduct]);
   const { setReportModalSlug } = props;
   const router = useRouter();
@@ -164,81 +189,27 @@ const ProductDetails: React.FC<Props> = (props) => {
 
       <div className="flex gap-x-[10px] items-center mb-6">
         <div className="flex">
-          <span className="text-gray-500">
-            <svg
-              width="18"
-              height="17"
-              viewBox="0 0 18 17"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M9 0L11.0206 6.21885H17.5595L12.2694 10.0623L14.2901 16.2812L9 12.4377L3.70993 16.2812L5.73056 10.0623L0.440492 6.21885H6.97937L9 0Z"
-                fill="#D2D8E1"
-              ></path>
-            </svg>
-          </span>
-          <span className="text-gray-500">
-            <svg
-              width="18"
-              height="17"
-              viewBox="0 0 18 17"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M9 0L11.0206 6.21885H17.5595L12.2694 10.0623L14.2901 16.2812L9 12.4377L3.70993 16.2812L5.73056 10.0623L0.440492 6.21885H6.97937L9 0Z"
-                fill="#D2D8E1"
-              ></path>
-            </svg>
-          </span>
-          <span className="text-gray-500">
-            <svg
-              width="18"
-              height="17"
-              viewBox="0 0 18 17"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M9 0L11.0206 6.21885H17.5595L12.2694 10.0623L14.2901 16.2812L9 12.4377L3.70993 16.2812L5.73056 10.0623L0.440492 6.21885H6.97937L9 0Z"
-                fill="#D2D8E1"
-              ></path>
-            </svg>
-          </span>
-          <span className="text-gray-500">
-            <svg
-              width="18"
-              height="17"
-              viewBox="0 0 18 17"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M9 0L11.0206 6.21885H17.5595L12.2694 10.0623L14.2901 16.2812L9 12.4377L3.70993 16.2812L5.73056 10.0623L0.440492 6.21885H6.97937L9 0Z"
-                fill="#D2D8E1"
-              ></path>
-            </svg>
-          </span>
-          <span className="text-gray-500">
-            <svg
-              width="18"
-              height="17"
-              viewBox="0 0 18 17"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M9 0L11.0206 6.21885H17.5595L12.2694 10.0623L14.2901 16.2812L9 12.4377L3.70993 16.2812L5.73056 10.0623L0.440492 6.21885H6.97937L9 0Z"
-                fill="#D2D8E1"
-              ></path>
-            </svg>
-          </span>
+          {avgRating && (
+            <ReactStars
+              count={5}
+              value={avgRating}
+              edit={false}
+              size={24}
+              isHalf={true}
+              emptyIcon={<FaRegStar />}
+              halfIcon={<FaStarHalfAlt />}
+              fullIcon={<FaStar />}
+              activeColor="rgb(255, 168, 0)"
+              color="#d3d3d3"
+            />
+          )}
         </div>
-        <span className="text-[13px] font-normal text-qblack">
-          {/* {props.itemDetail?.reviews?.length}  */}
-          Reviews
-        </span>
+        {totalReview && (
+          <span className="font-semibold text-[15px] font-normal text-qblack">
+            {" "}
+            {totalReview} Reviews
+          </span>
+        )}
       </div>
 
       <div className="flex gap-x-2 items-baseline mb-7">
